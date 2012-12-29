49c51443ae3744ad910b9e16c45e5e8d29b79589
Jeremia Kimelman <jbkimelman@gmail.com>
2012-12-28 17:03:01 -0800

    script up

--- wadl_to_iodocs.py
+++ wadl_to_iodocs.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python
+
 from BeautifulSoup import BeautifulSoup
 import re
 import simplejson as json
+import sys
 
 def wadl_to_rails_syntax(url):
     url = re.sub(r'{', r':', url)
@@ -30,53 +33,64 @@ def params_to_json(params):
     return param_list
 
 def method_to_json(method):
-    wadl_url = method.findAll('apigee:example')[0]['url']
-    uri = wadl_to_rails_syntax(wadl_url)
     j = {
             'MethodName': method['apigee:displayname'],
             'Synopsis': method.doc.text,
             'HTTPMethod': method['name'],
-            'URI': uri,
+            'URI': '',
             'RequiresOAuth': 'N',
             'parameters': [],
         }
 
-    params = method.findAll('param')
-    p = params_to_json(params)
+    print method.request
+    params = method.request.findAll('param')
+    
+    print len(params)
 
+    p = params_to_json(params)
     j['parameters'] = p
 
     return j
 
-wadl_file = open('tout-wadl.xml')
-json_file = open('tout.json', 'w+')
-
-soup = BeautifulSoup(wadl_file.read())
-
-resources = soup.findAll('resource')
-
-groups = {}
-tags = soup.findAll('apigee:tag', primary='true')
-for tag in tags:
-    p = tag.text
-    if p not in groups.keys():
-        groups[p] = []
-    else:
-        pass
-
-for resource in resources:
-    methods = resource.findAll('method')
-    for method in methods:
-        print method['apigee:displayname']
-        j = method_to_json(method)
-        group = method.findAll('apigee:tag', primary='true')[0].text
-        groups[group].append(j)
-
-endpoints = []
-for key in groups:
-    key_str = '%s related methods' % key
+if __name__=='__main__':
+    wf = sys.argv[1]
+    jf = sys.argv[2]
     
-    endpoints.append({'name': key_str, 'methods': groups[key]})
+    print wf
+    print jf
     
-data = json.dumps({'endpoints': endpoints }, sort_keys=True, indent=4)
-json_file.write(data)
+    wadl_file = open('%s' % wf)
+    json_file = open('%s' % jf, 'w+')
+
+    soup = BeautifulSoup(wadl_file.read())
+
+    print soup
+
+    resources = soup.findAll('resource')
+
+    groups = {}
+    tags = soup.findAll('apigee:tag', primary='true')
+    for tag in tags:
+        p = tag.text
+        if p not in groups.keys():
+            groups[p] = []
+        else:
+            pass
+
+    for resource in resources:
+        methods = resource.findAll('method')
+        for method in methods:
+            print method['apigee:displayname']
+            j = method_to_json(method)
+            j['URI'] = wadl_to_rails_syntax(resource['path'])
+            group = method.findAll('apigee:tag', primary='true')[0].text
+            groups[group].append(j)
+
+    endpoints = []
+    for key in groups:
+        key_str = '%s related methods' % key
+        
+        endpoints.append({'name': key_str, 'methods': groups[key]})
+        
+    data = json.dumps({'endpoints': endpoints }, sort_keys=True, indent=4)
+    json_file.write(data)
